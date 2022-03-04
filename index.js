54d82cd851b04bd2aab56ee77d6f5df4f31f3f8c
Eric Kalpin <elkalpin@gmail.com>
2022-03-03 19:04:51 -0600

    - Removed unnecessary semicolons

--- index.js
+++ index.js
@@ -15,21 +15,21 @@
 ////////////////////////////////////////
 // Packages
 ////////////////////////////////////////
-const express = require('express');
-const app = express();
-const serv = require('http').Server(app);
-const PORT = process.env.PORT || 3000;
+const express = require('express')
+const app = express()
+const serv = require('http').Server(app)
+const PORT = process.env.PORT || 3000
 
 ////////////////////////////////////////
 // Server setup
 ////////////////////////////////////////
 // the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname + '/game/public'));
+app.use(express.static(__dirname + '/game/public'))
 
 // listen for requests
 serv.listen(PORT, () => {
-    console.log(`Server listening for connections on port ${PORT}`);
-});
+    console.log(`Server listening for connections on port ${PORT}`)
+})
 
 ///////////////////////////////////////
 // Server started
@@ -38,8 +38,4 @@ console.log(
     `Web server has started!`,
     `This is NOT the game server`,
     `Link to game: http://localhost:${PORT}/index.html`
-);
-
-for (let i = 0; i < 20; i++) {
-    console.log('lolololololol')
-}
+)
