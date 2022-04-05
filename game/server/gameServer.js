ba95c06072c39124e85a52d1a2221eddd62dd3f0
Eric Kalpin <elkalpin@gmail.com>
2022-04-04 22:35:20 -0500

    - First attempt working on multiplayer

--- game/server/gameServer.js
+++ game/server/gameServer.js
@@ -4,8 +4,10 @@
 // multiplayer server for the game.
 ////////////////////////////////////////
 
-// Here we will use socket.io
+import BrainGame from "../public/brain/brainGame"
+const gameport  = 3001
 
+// Here we will use socket.io
 
 ////////////////////////////////////////
 // Sudo code for basic network communications
@@ -19,3 +21,20 @@
 //     const playerId = socket.connectionID // This does not support multiple players per client in networked games
 //     brain.brainComs.clientMessages[data.type]( data.args, playerId )
 // })
+
+class GameServer {
+    constructor() {
+        this.socket = require("socket.io")(gameport)
+        this.brain = new BrainGame({ isNetworked: true, network: this.socket})
+
+        // Incoming client messages
+        this.socket.on( 'genericClientMessage', ( data ) => {
+            const playerId = 0//socket.connectionID // This does not support multiple players per client in networked games
+            this.brain.brainComs.clientMessages[data.type]( data.args, playerId )
+        })
+
+        console.log('Created a new game server')
+    }
+}
+
+export default GameServer
--- index.js
+++ index.js
@@ -9,22 +9,33 @@
 //
 // The game's multiplayer server
 // entry point is:
-// `./game/server/server.js`
+// `./game/server/gameServer.js`
 ////////////////////////////////////////
 
+
+
 ////////////////////////////////////////
-// Packages
+// Packages ('import' requires node v13.2.0+ and `"type": "module"` to be in "package.json")
 ////////////////////////////////////////
+// import GameServer from './game/server/gameServer.js'
+
 const express = require('express')
 const app = express()
 const serv = require('http').Server(app)
 const PORT = process.env.PORT || 3000
+//const io = require('socket.io')(serv,{})
 
 ////////////////////////////////////////
 // Server setup
 ////////////////////////////////////////
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname + '/game/public'))
+
+////////////////////////////////////////
+// Multiplayer server setup (Move this eventaully?)
+////////////////////////////////////////
+
+// const gameServer = new GameServer(io)
 
 // listen for requests
 serv.listen(PORT, () => {
