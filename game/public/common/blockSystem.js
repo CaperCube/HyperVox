0abf8107c4243c9111f4cf5680ca2e9f6a240cde
Eric Kalpin <elkalpin@gmail.com>
2022-04-21 21:42:34 -0500

    - Added some new blocks!

--- game/public/common/blockSystem.js
+++ game/public/common/blockSystem.js
@@ -145,6 +145,108 @@ const blockTypes = [
         categories: [blockCats.decorative, blockCats.color.green],
         textures: { top: 210, bottom: 242, front: 226, back: 228, left: 227, right: 225 },
     },
+    // 0xc000
+    {
+        name: 'log',
+        categories: [blockCats.natural, blockCats.color.orange],
+        textures: { top: 13, bottom: 13, front: 12, back: 12, left: 12, right: 12 },
+    },
+    // 0xd000
+    {
+        name: 'leaves',
+        categories: [blockCats.natural, blockCats.transparent, blockCats.color.green],
+        textures: { top: 14, bottom: 14, front: 14, back: 14, left: 14, right: 14 },
+    },
+    // 0xe000
+    {
+        name: 'stone-pillar',
+        categories: [blockCats.color.grey, blockCats.decorative],
+        textures: { top: 16, bottom: 16, front: 15, back: 15, left: 15, right: 15 },
+    },
+    // 0xf000
+    {
+        name: 'stone-block-white',
+        categories: [blockCats.color.white, blockCats.decorative],
+        textures: { top: 16, bottom: 16, front: 16, back: 16, left: 16, right: 16 },
+    },
+    // 0x0100
+    {
+        name: 'stone-block-grey',
+        categories: [blockCats.color.grey, blockCats.decorative],
+        textures: { top: 32, bottom: 32, front: 32, back: 32, left: 32, right: 32 },
+    },
+    // 0x0200
+    {
+        name: 'stone-block-black',
+        categories: [blockCats.color.black, blockCats.decorative],
+        textures: { top: 48, bottom: 48, front: 48, back: 48, left: 48, right: 48 },
+    },
+    // 0x0300
+    {
+        name: 'stone-block-red',
+        categories: [blockCats.color.red, blockCats.decorative],
+        textures: { top: 64, bottom: 64, front: 64, back: 64, left: 64, right: 64 },
+    },
+    // 0x0400
+    {
+        name: 'stone-block-orange',
+        categories: [blockCats.color.orange, blockCats.decorative],
+        textures: { top: 80, bottom: 80, front: 80, back: 80, left: 80, right: 80 },
+    },
+    // 0x0500
+    {
+        name: 'stone-block-yellow',
+        categories: [blockCats.color.yellow, blockCats.decorative],
+        textures: { top: 96, bottom: 96, front: 96, back: 96, left: 96, right: 96 },
+    },
+    // 0x0600
+    {
+        name: 'stone-block-green',
+        categories: [blockCats.color.green, blockCats.decorative],
+        textures: { top: 112, bottom: 112, front: 112, back: 112, left: 112, right: 112 },
+    },
+    // 0x0700
+    {
+        name: 'stone-block-blue',
+        categories: [blockCats.color.blue, blockCats.decorative],
+        textures: { top: 128, bottom: 128, front: 128, back: 128, left: 128, right: 128 },
+    },
+    // 0x0800
+    {
+        name: 'stone-block-indigo',
+        categories: [blockCats.color.indigo, blockCats.decorative],
+        textures: { top: 144, bottom: 144, front: 144, back: 144, left: 144, right: 144 },
+    },
+    // 0x0900
+    {
+        name: 'stone-block-violet',
+        categories: [blockCats.color.violet, blockCats.decorative],
+        textures: { top: 160, bottom: 160, front: 160, back: 160, left: 160, right: 160 },
+    },
+    // 0x0a00
+    {
+        name: 'water',
+        categories: [blockCats.natural, blockCats.transparent, blockCats.color.blue],
+        textures: { top: 18, bottom: 18, front: 18, back: 18, left: 18, right: 18 },
+    },
+    // 0x0b00
+    {
+        name: 'lava',
+        categories: [blockCats.natural, blockCats.damaging, blockCats.color.red],
+        textures: { top: 34, bottom: 34, front: 34, back: 34, left: 34, right: 34 },
+    },
+    // 0x0c00
+    {
+        name: 'stone',
+        categories: [blockCats.natural, blockCats.color.grey],
+        textures: { top: 21, bottom: 21, front: 21, back: 21, left: 21, right: 21 },
+    },
+    // 0x0d00
+    {
+        name: 'stone-clumpy',
+        categories: [blockCats.color.grey],
+        textures: { top: 22, bottom: 22, front: 22, back: 22, left: 22, right: 22 },
+    },
 ]
 
 export {
